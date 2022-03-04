54412d751f146d41d0cec04cac99c5b5d5faef45
Adam Divall <adivall@sky.com>
2022-03-04 12:40:52 +0000

    Major Revision of Code

--- index.py
+++ index.py
@@ -1,10 +1,21 @@
 import boto3
 import os
+import json
 import cfnresponse
+from botocore.config import Config
 from botocore.exceptions import ClientError
 
 security_hub_master_account_id=os.environ['SECURITY_HUB_MASTER_ACCOUNT_ID']
+security_hub_state_machine_arn=os.environ['SECURITY_HUB_STATE_MACHINE_ARN']
+log_archive_account_id=os.environ['LOG_ARCHIVE_ACCOUNT_ID']
 role_to_assume=os.environ['ROLE_TO_ASSUME']
+config=Config(
+    retries={
+        'max_attempts':10,
+        'mode':'adaptive'
+    }
+)
+stepfunction_client = boto3.client('stepfunctions')
 
 def lambda_handler(event, context):
     security_hub_regions=boto3.Session().get_available_regions('securityhub')
@@ -16,8 +27,8 @@ def lambda_handler(event, context):
             try:
                 for region in control_tower_regions:
                     if region in security_hub_regions:
-                        enable_security_hub_master(security_hub_master_account_session, region)
-                        enable_security_hub_member(security_hub_master_account_session, region, accounts)
+                        ct_home_region=enable_security_hub_master(security_hub_master_account_session, region)
+                        enable_security_hub_member(security_hub_master_account_session, region, accounts, ct_home_region)
             except ClientError as e: 
                 print(e.response['Error']['Message']) 
                 cfnresponse.send(event, context, cfnresponse.FAILED, e.response)
@@ -35,17 +46,10 @@ def lambda_handler(event, context):
     else:
         for region in control_tower_regions:
             if region in security_hub_regions:
-                enable_security_hub_master(security_hub_master_account_session, region)
-                enable_security_hub_member(security_hub_master_account_session, region, accounts)     
+                ct_home_region=enable_security_hub_master(security_hub_master_account_session, region)
+                enable_security_hub_member(security_hub_master_account_session, region, accounts, ct_home_region)     
 
 def get_control_tower_regions():
-    """
-    Description:
-        Finds the AWS Control Tower governed regions by Identifying the AWS Regions used within the AWS CloudFormation StackSets deployed by AWS Control Tower.
-    Returns:
-        List of AWS Control Tower governed regions.
-    """
-    
     cloudformation_client=boto3.client('cloudformation')
     control_tower_regions=set()
     try:
@@ -61,23 +65,11 @@ def get_control_tower_regions():
     return list(control_tower_regions)
 
 def assume_role(aws_account_id, role_to_assume):
-    """
-    Description:
-        Assumes the provided role in the specified AWS Account and returns a GuardDuty Client.
-    Parameters:
-        "aws_account_id" = AWS Account Number.
-        "role_to_assume" = Role to assume in target account.
-    Returns:
-        Security Hub Client in the specified AWS Account and AWS Region.
-    """
-
-    # Beginning the AssumeRole process for the Account.
     sts_client=boto3.client('sts')
     response=sts_client.assume_role(
         RoleArn=f'arn:aws:iam::{aws_account_id}:role/{role_to_assume}',
         RoleSessionName='EnableSecurityHub'
     )
-    # Storing STS Credentials.
     sts_session=boto3.Session(
         aws_access_key_id=response['Credentials']['AccessKeyId'],
         aws_secret_access_key=response['Credentials']['SecretAccessKey'],
@@ -87,13 +79,6 @@ def assume_role(aws_account_id, role_to_assume):
     return sts_session
 
 def get_all_accounts():
-    """
-    Description:
-        Get all Accounts within the AWS Organization.
-    Returns:
-        A list of all AWS Accounts in the AWS Organization that have a Status of 'ACTIVE'.
-    """
-
     org_client=boto3.client('organizations')
     all_accounts=[]
     active_accounts=[]
@@ -114,14 +99,14 @@ def get_all_accounts():
 
 def enable_security_hub_master(security_hub_master_account_session, region):
     cloudtrail_client=boto3.client('cloudtrail')
-    cloudtrail_response=cloudtrail_client.describe_trails(
+    ct_home_region=cloudtrail_client.describe_trails(
         trailNameList=[
             'aws-controltower-BaselineCloudTrail',
         ]
-    )
-    control_tower_home_region=cloudtrail_response['trailList'][0]['HomeRegion']
-    sh_client=boto3.client('securityhub', region_name=region)
-    delegated_admin_client=security_hub_master_account_session.client('securityhub', region_name=region)
+    )['trailList'][0]['HomeRegion']
+    sh_client=boto3.client('securityhub', region_name=region, config=config)
+    config_client=security_hub_master_account_session.client('config', region_name=region)
+    delegated_admin_client=security_hub_master_account_session.client('securityhub', region_name=region, config=config)
     delegated_admin_account=sh_client.list_organization_admin_accounts()
     try:
         delegated_admin_client.get_findings()
@@ -146,20 +131,49 @@ def enable_security_hub_master(security_hub_master_account_session, region):
     delegated_admin_client.update_organization_configuration(
         AutoEnable=True
     )
-    process_security_standards(delegated_admin_client, region, security_hub_master_account_id)
+    if region == ct_home_region:
+        finding_aggregators=delegated_admin_client.list_finding_aggregators()['FindingAggregators']
+        if len(finding_aggregators) > 0:
+            print(f"Security Hub already has a Findings Aggregator in Account ID: {security_hub_master_account_id}.")
+        else:
+            if region == ct_home_region:
+                delegated_admin_client.create_finding_aggregator(
+                    RegionLinkingMode='ALL_REGIONS'
+                )
+                print(f"Security Hub Findings Aggregator created in Account ID: {security_hub_master_account_id}.")
+            else:
+                print(f"Current Region {region} is not the Control Tower Home Region.")
+    else:
+        config_recorder=config_client.describe_configuration_recorders()
+        try:
+            config_client.put_configuration_recorder(
+                ConfigurationRecorder={
+                    'name': config_recorder['ConfigurationRecorders'][0]['name'],
+                    'roleARN': config_recorder['ConfigurationRecorders'][0]['roleARN'],
+                    'recordingGroup': {
+                        'allSupported': True,
+                        'includeGlobalResourceTypes': False 
+                    }
+                }
+            )
+            print(f"Excluded Global Resources from AWS Config in Region {region} in Account ID: {security_hub_master_account_id}.")
+        except Exception as e:
+            print(f"AWS Config is not Enable in Account ID: {security_hub_master_account_id} in Region: {region}.")
+    return ct_home_region
 
-def enable_security_hub_member(security_hub_master_account_session, region, accounts):
-    delegated_admin_client=security_hub_master_account_session.client('securityhub', region_name=region)
+def enable_security_hub_member(security_hub_master_account_session, region, accounts, ct_home_region):
+    delegated_admin_client=security_hub_master_account_session.client('securityhub', region_name=region, config=config)
     for account in accounts:
+        member_session=assume_role(account['Id'], role_to_assume)
+        member_client=member_session.client('securityhub', region_name=region, config=config)
         if account['Id'] != security_hub_master_account_id:
-            member_session=assume_role(account['Id'], role_to_assume)
-            member_client=member_session.client('securityhub', region_name=region)
             try:
                 member_client.enable_security_hub(
                     EnableDefaultStandards=False
                 )
+                print(f"Enabled Security Hub in Account ID: {account['Id']} in Region {region}.")
             except Exception:
-                print(f"Security Hub is enabled in Account ID: {account['Id']} in region {region}.")
+                print(f"Security Hub is already enabled in Account ID: {account['Id']} in Region {region}.")
             try:
                 delegated_admin_client.create_members(
                     AccountDetails=[
@@ -171,113 +185,24 @@ def enable_security_hub_member(security_hub_master_account_session, region, acco
                 )
             except Exception:
                 print(f"Account ID: {account['Id']} is already associated with Security Hub Admin Account.")
-            process_security_standards(member_client, region, account['Id'])
-
-def process_security_standards(member_client, region, account_id):
-    aws_foundations_arn=(f"arn:aws:securityhub:{region}::standards/aws-foundational-security-best-practices/v/1.0.0")
-    aws_foundations_subscription_arn=(f"arn:aws:securityhub:{region}:{account_id}:subscription/aws-foundational-security-best-practices/v/1.0.0")
-    cis_foundations_arn=("arn:aws:securityhub:::ruleset/cis-aws-foundations-benchmark/v/1.2.0")
-    cis_foundations_subscription_arn=(f"arn:aws:securityhub:{region}:{account_id}:subscription/cis-aws-foundations-benchmark/v/1.2.0")
-    pcidss_arn=(f"arn:aws:securityhub:{region}::standards/pci-dss/v/3.2.1")
-    pcidss_subscription_arn=(f"arn:aws:securityhub:{region}:{account_id}:subscription/pci-dss/v/3.2.1")
-    aws_foundations_enabled=False
-    cis_foundations_enabled=False
-    pcidss_enabled=False
-    enabled_standards=member_client.get_enabled_standards()
-    for item in enabled_standards["StandardsSubscriptions"]:
-        if aws_foundations_arn in item["StandardsArn"]:
-            aws_foundations_enabled = True
-        if cis_foundations_arn in item["StandardsArn"]:
-            cis_foundations_enabled = True
-        if pcidss_arn in item["StandardsArn"]:
-            pcidss_enabled = True
-    if os.environ['AWS_FOUNDATIONS'] == 'Yes':
-        if aws_foundations_enabled:
-            print(f"AWS Foundational Security Best Practices v1.0.0 Security Standard is already enabled in Account ID: {account_id} in {region}.")
-        else:
-            try:
-                member_client.batch_enable_standards(
-                    StandardsSubscriptionRequests=[
-                        {
-                            'StandardsArn': aws_foundations_arn
-                        }
-                    ])
-                print(f"Enabled AWS Foundational Security Best Practices v1.0.0 Security Standard in Account ID: {account_id} in {region}.")
-            except Exception as e:
-                print(f"Failed to enable AWS Foundational Security Best Practices v1.0.0 Security Standard in Account ID: {account_id} in {region}.")
-    # Disable AWS Standard
-    else:
-        if not aws_foundations_enabled:
-            print(f"AWS Foundational Security Best Practices v1.0.0 Security Standard is already disabled in Account ID: {account_id} in {region}.")
-        else:
-            try:
-                member_client.batch_disable_standards(
-                    StandardsSubscriptionArns=[
-                        aws_foundations_subscription_arn
-                    ]
-                )
-                print(f"Disabled AWS Foundational Security Best Practices v1.0.0 Security Standard in Account ID: {account_id} in {region}.")
-            except Exception as e:
-                print(f"Failed to disable AWS Foundational Security Best Practices v1.0.0 Security Standard in Account ID: {account_id} in {region}.")
-    if os.environ['CIS_FOUNDATIONS'] == 'Yes':
-        if cis_foundations_enabled:
-            print(f"CIS AWS Foundations Benchmark v1.2.0 Security Standard is already enabled in Account ID: {account_id} in {region}.")
-        else:
-            try:
-                member_client.batch_enable_standards(
-                    StandardsSubscriptionRequests=[
-                        {
-                            'StandardsArn': cis_foundations_arn
-                        }
-                    ])
-                print(f"Enabled CIS AWS Foundations Benchmark v1.2.0 Security Standard in Account ID: {account_id} in {region}.")
-            except Exception as e:
-                print(f"Failed to enable CIS AWS Foundations Benchmark v1.2.0 Security Standard in Account ID: {account_id} in {region}.")
-    # Disable AWS Standard
-    else:
-        if not cis_foundations_enabled:
-            print(f"CIS AWS Foundations Benchmark v1.2.0 Security Standard is already disabled in Account ID: {account_id} in {region}.")
-        else:
-            try:
-                member_client.batch_disable_standards(
-                    StandardsSubscriptionArns=[
-                        cis_foundations_subscription_arn
-                    ]
-                )
-                print(f"Disabled CIS AWS Foundations Benchmark v1.2.0 Security Standard in Account ID: {account_id} in {region}.")
-            except Exception as e:
-                print(f"Failed to disable CIS AWS Foundations Benchmark v1.2.0 Security Standard in Account ID: {account_id} in {region}.")
-    if os.environ['PCIDSS'] == 'Yes':
-        if pcidss_enabled:
-            print(f"PCI DSS v3.2.1 Security Standard is already enabled in Account ID: {account_id} in {region}.")
-        else:
-            try:
-                member_client.batch_enable_standards(
-                    StandardsSubscriptionRequests=[
-                        {
-                            'StandardsArn': pcidss_arn
-                        }
-                    ])
-                print(f"Enabled PCI DSS v3.2.1 Security Standard in Account ID: {account_id} in {region}.")
-            except Exception as e:
-                print(f"Failed to enable PCI DSS v3.2.1 Security Standard in Account ID: {account_id} in {region}.")
-    # Disable AWS Standard
-    else:
-        if not pcidss_enabled:
-            print(f"PCI DSS v3.2.1 Security Standard is already disabled in Account ID: {account_id} in {region}.")
-        else:
-            try:
-                member_client.batch_disable_standards(
-                    StandardsSubscriptionArns=[
-                        pcidss_subscription_arn
-                    ]
-                )
-                print(f"Disabled PCI DSS v3.2.1 Security Standard in Account ID: {account_id} in {region}.")
-            except Exception as e:
-                print(f"Failed to disable PCI DSS v3.2.1 Security Standard in Account ID: {account_id} in {region}.")
+        security_hub_state_machine_input={
+            "SecurityHubMasterAccountId":security_hub_master_account_id,
+            "LogArchiveAccountId":log_archive_account_id,
+            "Region":region,
+            "ControlTowerHomeRegion":ct_home_region,
+            "AccountId":account['Id'],
+            "RoleToAssume":role_to_assume
+        }
+        try:
+            stepfunction_client.start_execution(
+                stateMachineArn=security_hub_state_machine_arn,
+                input=json.dumps(security_hub_state_machine_input)
+            )
+        except Exception as e:
+            print(f"Unable to Trigger Enabling of Security Hub in Account ID: {account['Id']} in Region {region}. Error: {e}.")
 
 def disable_security_hub(security_hub_master_account_session, region, accounts):
-    sh_client=boto3.client('securityhub', region_name=region)
+    sh_client=boto3.client('securityhub', region_name=region, config=config)
     try:
         sh_client.disable_organization_admin_account(
             AdminAccountId=security_hub_master_account_id
@@ -285,17 +210,18 @@ def disable_security_hub(security_hub_master_account_session, region, accounts):
         print(f"Disabled Delegated Admin for Security Hub in Account ID: {security_hub_master_account_id}.")
     except Exception:
         print(f"Delegated Admin is not configured.")
-    delegated_admin_client=security_hub_master_account_session.client('securityhub', region_name=region)
+    delegated_admin_client=security_hub_master_account_session.client('securityhub', region_name=region, config=config)
     member_accounts=[]
     for account in accounts:
         if account['Id'] != security_hub_master_account_id:        
             member_session=assume_role(account['Id'], role_to_assume)
-            member_client=member_session.client('securityhub', region_name=region)
+            member_client=member_session.client('securityhub', region_name=region, config=config)
             member_accounts.append(account['Id'])
             try:
                 member_client.disable_security_hub()
+                print(f"Disabled Security Hub in Account ID: {account['Id']} in Region {region}.")
             except Exception:
-                print(f"Failed to disable AWS Security Hub in Account ID: {account['Id']} in region {region}.")
+                print(f"Failed to disable Security Hub in Account ID: {account['Id']} in region {region}.")
     delegated_admin_client.disassociate_members(AccountIds=member_accounts)
     print(f"Disassociated Member Accounts from the Security Hub Admin Account in {region}.")
     delegated_admin_client.delete_members(AccountIds=member_accounts)
--- readme.md
+++ readme.md
@@ -7,13 +7,17 @@ The reason I've adapted it is that I noticed a few things:
     * From my perspective, that could potentially add additional costs to the overarching bill.
     * There maybe situations where Clients restrict the AWS Regions that can be used within AWS e.g. through Service Control Policies. 
     * Control Tower may not have all AWS Regions governed by Control Tower meaning that effectively other AWS Best Practices may not be configured within those regions.
-    * **Change Made:** The code has been tweaked to identify the Control Tower governed regions by listing the regions utilised from a deployed CloudFormation StackSet which occurs on any Control Tower deployment.  This is in light of the fact that there is no API for Control Tower.  There is also a slight change in the codes' logic to loop through all regions governed by Control Tower and if this is a supported region by GuardDuty to then proceed as per the orginal code. 
+    * **Change Made:** The `get_control_tower_regions` python function has been created to identify the Control Tower governed regions by listing the regions utilised from a deployed CloudFormation StackSet `AWSControlTowerBP-BASELINE-CONFIG` which occurs on any Control Tower deployment.  This is in light of the fact that there is no API for Control Tower.  The lambda function logic has been adjusted to loop through all regions governed by Control Tower first and if this is a supported region by Security Hub to then proceed. 
     * **Change Made:** The `get_all_accounts` python function has been updated to only return AWS Accounts in the Organisation that have a status of 'ACTIVE'. Therefore any AWS Accounts that are in a suspended state pending deletion are ignored.
 2.  Once deployed, Security Hub wasn't configured with a Delegated Admin or to auto-invite AWS Accounts within the Organisation.
-    * **Change Made:** The `enable_security_hub_admin` python function has been updated to use the `enable_organization_admin_account` and `update_organization_configuration` boto3 API's to ensure that a Delegated Admin is configured and that Auto-Enable is also turned on
-3.  If you try to delete the AWS CloudFormation Stack that is created following on from the enabling of a Delegated Admin then it would fail to disable Security Hub.
+    * **Change Made:** The `enable_security_hub_admin` python function has been updated to use the `enable_organization_admin_account` and `update_organization_configuration` boto3 API's to ensure that a Delegated Admin is configured and that Auto-Enable is also turned on.
+3.  For each AWS Account Security Hub was enabled in every region and therefore there was duplication of some controls that ultimately increase the costs of running Security Hub and in turn AWS Config as well.  For example: IAM Controls only need to be checked in a Single Region per AWS Account therefore duplication of the Config Rule is providing no additional value.
+    * **Change Made:** The `enable_security_hub_admin` python function has been updated to create a findings aggregator for all regions to send to the Control Tower Home Region.
+    * **Change Made:** Both the `enable_security_hub_admin` and the `enable_security_hub_members` python functions have been updated to disable Config Global Recording for any region that isn't the Control Tower Home Region.
+    * **Change Made:** The `Lambda-Security-Hub-Disable-AWS-Foundations-Global-Controls`, `Lambda-Security-Hub-Disable-CIS-Foundations-Global-Controls` and `Lambda-Security-Hub-Disable-PCIDSS-Global-Controls` Lambda Functions have been created to disable duplicated Global Controls as per the [Best Practices for cross-region aggregation of security findings in Security Hub](https://aws.amazon.com/blogs/security/best-practices-for-cross-region-aggregation-of-security-findings/). Specifically the controls that have been disabled are as per the documentation for each of the Security Standards [AWS Foundational Best Practices controls that you might want to disable](https://docs.aws.amazon.com/securityhub/latest/userguide/securityhub-standards-fsbp-to-disable.html), [CIS AWS Foundations Benchmark controls that you might want to disable](https://docs.aws.amazon.com/securityhub/latest/userguide/securityhub-standards-cis-to-disable.html) and [PCI DSS controls that you might want to disable](https://docs.aws.amazon.com/securityhub/latest/userguide/securityhub-standards-pcidss-to-disable.html). Considering the issues of scaling I've leveraged Step Functions for the process of executing multiple accounts at the same time and the use of exponential backoff within the code to ensure that it doesn't run into API throttling issues.
+4.  If you try to delete the AWS CloudFormation Stack that is created following on from the enabling of a Delegated Admin then it would fail to disable Security Hub.
     * **Change Made:** The `disable_security_hub` python function has been updated to loop through all Members and deregister the Delegated Admin and then loop through all Active AWS Accounts and disabling Security Hub in all configured regions.
-4.  Added the Capabiity for both newly created AWS Accounts through the Account Vending Maching component of Control Tower or existing AWS Accounts that have been joined to the AWS Organization and registered in Control Tower to have Security Hub enabled.
+5.  Added the Capabiity for both newly created AWS Accounts through the Account Vending Maching component of Control Tower or existing AWS Accounts that have been joined to the AWS Organization and registered in Control Tower to have Security Hub enabled.
     * **Change Made:** Update to the CloudFormation Template to create an EventBridge Rule that is triggered based on a Control Tower LifeCycle Event `CreateManagedAccount`.
 
 ## Architecture Overview
@@ -27,7 +31,7 @@ The reason I've adapted it is that I noticed a few things:
 There is an overarching assumption that you already have [Customisation for Control Tower](https://aws.amazon.com/solutions/implementations/customizations-for-aws-control-tower/) deployed within your Control Tower Environment.
 
 1.  Clone the GitHub Repo to your local device.
-2.  Create an S3 Bucket where you'll then upload the `securityhub.zip` file to. Make a note of the bucket name and the prefix to the `guarduty.zip`. Note: The region where you create the bucket will need to be in the region of the Control Tower home region since that is where the Lambda Function will be created.
+2.  Create an S3 Bucket where you'll then upload the `securityhub.zip` file to. Make a note of the bucket name and the prefix to the `securityhub.zip`. Note: The region where you create the bucket will need to be in the region of the Control Tower home region since that is where the Lambda Function will be created.
 
 ### Installation
 
@@ -36,6 +40,7 @@ There is an overarching assumption that you already have [Customisation for Cont
 3.  Update the CloudFormation Parameters `enable-securityhub.json` with the required details:
     * **OrganizationId:** This is used to implement conditions within the IAM Policy used for the Lambda Execution Role. This can be obtained from with AWS Organisations.
     * **SecurityHubMasterAccountId:** This is the AWS Account ID of the Account that you wish to configure as the delegated admin for Security Hub.  It's recommended to use the Security Account (formerly called Audit Account) configured by Control Tower.
+    * **LogArchiveAccountId:** This is the AWS Account ID of the Account that has been configured as the delegated admin for Security Hub.  It's recommended to use the Security Account (formerly called Audit Account) configured by Control Tower.
     * **S3SourceBucket:** This is the S3 Bucket where the Lambda Function source files are located. 
     * **S3Key:** This is the prefix within the S3 Bucket where the Lambda Function source files are located. 
     * **RoleToAssume:** This is used within the Lambda Function to AssumeRole into other AWS Accounts in order to Create/Configure/Delete different AWS Services such as Security Hub.  This is preconfigured with a default value of `AWSControlTowerExecution` since this IAM Role is created in all AWS Accounts as part the AWS Control Tower setup.
@@ -55,6 +60,10 @@ There is an overarching assumption that you already have [Customisation for Cont
             "ParameterKey": "SecurityHubMasterAccountId",
             "ParameterValue": ""
         },
+        {
+            "ParameterKey": "LogArchiveAccountId",
+            "ParameterValue": ""
+        },        
         {
             "ParameterKey": "S3SourceBucket",
             "ParameterValue": ""
